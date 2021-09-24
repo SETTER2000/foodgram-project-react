f1dca816f6472fd7150d9db2326c923aee23faa8
SETTER <info@lphp.ru>
2021-09-24 14:04:25 +0300

    auth djoser

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -6,10 +7,13 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('djoser.urls')),
+
     path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include('backend.users.urls')),
-    # path('api/auth/token/login/', include('djoser.urls.jwt')),
+    # если раскомментить будет работать регистрация, НО перестанет работать
+    # правильно остальное API.
+    # path('api/', include('backend.users.urls')),
+    path('api/', include('djoser.urls')),
+
     # path(
     #     'docs/',
     #     TemplateView.as_view(template_name='redoc.html'),
