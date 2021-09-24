a77df6687bb8ceecfd2c8c9be66f100ab82cc8c6
SETTER <info@lphp.ru>
2021-09-24 20:10:47 +0300

    Err

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -37,3 +37,22 @@ class PermissonForRole(BasePermission):
                     or request.method in self.roles_permissions[
                         request.user.role])
         return request.method in self.roles_permissions['anon']
+
+
+def is_authenticated(user):
+    pass
+
+
+class AllowPostAnyReadAuthenticatedUser(BasePermission):
+
+    def has_permission(self, request, view):
+        # Allow anyone to register
+        if request.method == "POST":
+            return True
+        # Must be authenticated to view
+        else:
+            return request.user and is_authenticated(request.user)
+
+    def has_object_permission(self, request, view, obj):
+        # Any view method requires you to be the user
+        return obj.id == request.user.id or request.user.is_superuser
