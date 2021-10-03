7496aeab14cbeed374b4b88b520e70b483c964a9
SETTER <lphp@mail.ru>
2021-10-03 03:27:38 +0300

    sv

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -2,7 +2,7 @@ import os, base64, time
 
 from foodgram.settings import MEDIA_ROOT, SUB_DIR_RECIPES
 from .models import (Ingredient, Tag, Recipes, User, Favorite,
-                     IngredientOfRecipes)
+                     IngredientRecipes)
 from rest_framework import serializers
 
 
@@ -23,8 +23,7 @@ class Base64ImageFieldToFile(serializers.Field):
     def to_internal_value(self, data):
         """Для записи."""
 
-        dir = f'{MEDIA_ROOT}/{SUB_DIR_RECIPES}'
-        os.makedirs(dir, exist_ok=True)
+        os.makedirs(f'{MEDIA_ROOT}/{SUB_DIR_RECIPES}', exist_ok=True)
 
         try:
             format, imgstr = data.split(';base64,')
@@ -59,14 +58,13 @@ class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
-        fields = ('id', 'name', 'measurement_unit')
+        fields = ('name', 'measurement_unit')
 
     # def get_amount(self, obj):
     #     return obj.amount
 
 
 class TagSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
@@ -79,11 +77,10 @@ class RecipesSerializer(serializers.ModelSerializer):
     # author = serializers.StringRelatedField(read_only=True)
     image = Base64ImageFieldToFile()
 
-    # ingredients = IngredientSerializer(read_only=True, many=True)
+    ingredients = IngredientSerializer(many=True)
 
     class Meta:
         model = Recipes
-        # read_only_fields = ('name',)
         fields = (
             'id',
             'name',
@@ -96,18 +93,25 @@ class RecipesSerializer(serializers.ModelSerializer):
             'is_in_shopping_cart',
             'cooking_time')
 
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipes.objects.create(**validated_data)
+        for ingredient_data in ingredients_data:
+            Ingredient.objects.create(recipe=recipe, **ingredient_data)
+        return recipe
+
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation["author"] = AuthorSerializer(instance.author).data
         return representation
 
-    def create(self, validated_data):
-        tracks_data = validated_data.pop('tags')
-        recipe = Recipes.objects.create(**validated_data)
-        for track_data in tracks_data:
-            Tag.objects.create(recipe=recipe, **track_data)
-        return recipe
-
+    #  def create(self, validated_data):
+    #     tracks_data = validated_data.pop('tags')
+    #     recipe = Recipes.objects.create(**validated_data)
+    #     for track_data in tracks_data:
+    #         Tag.objects.create(recipe=recipe, **track_data)
+    #     return recipe
 
     # def create(self, validated_data):
     #     # Если в исходном запросе не было поля ingredients
@@ -130,6 +134,6 @@ class RecipesSerializer(serializers.ModelSerializer):
     #             **ingredient)
     #         # Поместим ссылку на каждый ингредиент во вспомогательную таблицу
     #         # Не забыв указать к какому рецепту он относится
-    #         IngredientOfRecipes.objects.create(
+    #         IngredientRecipes.objects.create(
     #             ingredient=current_ingredient, recipe=recipe)
     #     return recipe
