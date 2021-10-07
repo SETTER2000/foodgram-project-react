1021872803d73dec3a66b56b84dd361e4ce01e2c
SETTER <lphp@mail.ru>
2021-10-08 01:33:05 +0300

    Льключен фильтр по тегам. Конфликтует с пагинацией, не правильно фильтрует объекты.

--- backend/app/pagination.py
+++ backend/app/pagination.py
@@ -1,7 +1,16 @@
-class PaginatedCSVRenderer(CSVRenderer):
-    results_field = 'results'
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-    def render(self, data, *args, **kwargs):
-        if not isinstance(data, list):
-            data = data.get(self.results_field, [])
-        return super(PaginatedCSVRenderer, self).render(data, *args, **kwargs)
+from backend.app.models import Tag
+from backend.app.serializers import TagSerializer
+
+
+class PaginationAll(PageNumberPagination):
+    page_size = 6
+    max_page_size = 300
+
+
+class PaginationNull(PageNumberPagination):
+    """Без пагинации."""
+    def get_paginated_response(self, data):
+        return Response(data)
