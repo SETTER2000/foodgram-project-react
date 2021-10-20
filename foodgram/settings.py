2cb0be77d05348aa2f6782bb70cfef6d7a9f8fe8
alex <apetrov@landata.ru>
2021-10-20 11:16:02 +0300

    fss

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -25,7 +25,7 @@ EVD = '-пусто-'
 DEBUG = False
 
 # ALLOWED_HOSTS = [host for host in os.environ.get('ALLOWED_HOSTS')]
-ALLOWED_HOSTS = ['178.154.193.199', 'localhost', 'kino2000.ru', 'www.kino2000.ru']
+ALLOWED_HOSTS = ['*']
 
 FONT_PDF = os.environ.get('FONT_PDF')
 
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': os.environ.get('DB_ENGINE'),
+        # 'NAME': os.environ.get('DB_NAME'),
+        # 'USER': os.environ.get('POSTGRES_USER'),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        # 'HOST': os.environ.get('DB_HOST'),
+        # 'PORT': os.environ.get('DB_PORT'),
     }
 }
 
