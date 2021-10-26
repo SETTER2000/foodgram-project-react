08e81765d521a1a7fb8a6437fc396eccf843ba3d
SETTER <lphp@mail.ru>
2021-10-26 03:11:16 +0300

    Son

--- backend/app/apps.py
+++ backend/app/apps.py
@@ -2,9 +2,9 @@ from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app"
-    verbose_name = "Рецепты"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app'
+    verbose_name = 'Рецепты'
 
     def ready(self):
         import app.signals  # Noqa
--- backend/app/pagination.py
+++ backend/app/pagination.py
@@ -16,5 +16,5 @@ class PaginationNull(PageNumberPagination):
 
 class FoodgramPagination(PageNumberPagination):
     page_size = 10
-    page_query_param = "page"
-    page_size_query_param = "limit"
+    page_query_param = 'page'
+    page_size_query_param = 'limit'
