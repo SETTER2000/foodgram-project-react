779d0a6d10399a3b9246dcc237cf1b2c585db2b1
SETTER <lphp@mail.ru>
2021-11-01 21:45:10 +0300

    Du3

--- backend/app/apps.py
+++ backend/app/apps.py
@@ -5,3 +5,6 @@ class AppsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
     verbose_name = 'Рецепты'
+
+    def ready(self):
+        import recipes.signals
--- backend/configuration/settings/prod.py
+++ backend/configuration/settings/prod.py
@@ -1,6 +1,6 @@
 import os
 
-# from .base import *
+from .base import *
 from .base import env
 
 DEBUG = False
