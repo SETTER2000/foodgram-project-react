f1194bc06e41ded26361f824b16641801a44f993
SETTER <lphp@mail.ru>
2021-11-15 03:16:56 +0300

    change forkflow

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import TextField
