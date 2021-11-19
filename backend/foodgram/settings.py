299cdf21af237f10f0cac60a52b01b9a9ad4025b
SETTER <lphp@mail.ru>
2021-11-20 02:17:01 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -123,12 +123,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = ((os.path.join(BASE_DIR,
 #                                   'frontend/build/../../frontend/build/static')),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
